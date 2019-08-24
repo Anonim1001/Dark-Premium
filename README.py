b65c0c66c5cc1e3eef264e35249b7df8bc2f17d7
Cyber HallHoll <53478580+Anonim1001@users.noreply.github.com>
2019-08-24 10:54:15 +0700

    Add files via upload

---
+++ README.py
@@ -0,0 +1,13 @@
+# Dark-Mod2
+
+
+$ pkg upgrade && pkg update 
+$ pkg install python2
+$ pip2 install requests
+$ pip2 install mechanize
+$ pkg install git
+$ git clone https://github.com/Anonim1001/Dark-Premium
+$ ls
+$ cd Dark
+$ ls
+$ python2 Dark.py
